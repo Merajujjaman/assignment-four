446a37c5b4ed2ebaebdaf8558b65d39ff51a6c85
Merajujjaman <asmmerajujjaman@gmail.com>
2023-02-08 04:12:43 +0600

    1st problem

---
+++ index.js
@@ -0,0 +1,10 @@
+/*Problem 1: Let’s play a mind game------->
+if you input a number then this function will do this operation ((number * 3 + 10) / 2) - 5
+-----------------------------------------------------------------------------------*/
+function mindGame(number1) {
+    return (((number1 * 3 + 10) / 2) - 5);
+}
+const hello = mindGame(33);
+console.log(hello);
+
+  
